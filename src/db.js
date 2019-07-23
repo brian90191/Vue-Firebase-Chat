a0104b3e0f9a5402db2a3de6dda2e59b0dfeb37a
brian <brian90191@gmail.com>
2019-07-23 16:25:34 +0800

    將 firebase 設定改由 .env 讀取

--- src/db.js
+++ src/db.js
@@ -4,13 +4,13 @@ import 'firebase/auth'
 
 /* firebase */
 const firebaseConfig = {
-    apiKey: "AIzaSyBLkR4b40WH8Z0TrfZ_PsMnvZxA2u5A7pE",
-    authDomain: "chat-firebase-69076.firebaseapp.com",
-    databaseURL: "https://chat-firebase-69076.firebaseio.com",
-    projectId: "chat-firebase-69076",
-    storageBucket: "chat-firebase-69076.appspot.com",
-    messagingSenderId: "151991030717",
-    appId: "1:151991030717:web:c3f61d4ddae60e28"
+    apiKey: process.env.VUE_APP_API_KEY,
+    authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+    databaseURL: process.env.VUE_APP_DATABASE_URL,
+    projectId: process.env.VUE_APP_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_APP_ID
 };
 
 // Get a Firestore instance
